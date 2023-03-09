dfca6ea0b2b0c8840259622fa2b6be192c587f8c
andystrn <an.strnadlova@gmail.com>
2023-03-09 10:15:03 +0100

    added Header component

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import smartHomeData from './smartHomeData';
+import Header from './components/Header';
+import {backGround} from './../html-vzor/images/house.jpg'
 
 const App = () => (
   <>
-
+  <div className="container">
+    <Header title={'Chytrá domácnost'}/>
+  </div>
   </>
 );
 
